6a0f47065fee2a891a649a5ad0413b3d5e1f4661
decalcomania22 <140874353+decalcomania22@users.noreply.github.com>
2024-03-25 16:35:13 +0530

    Update med.js

--- med.js
+++ med.js
@@ -1,44 +1,44 @@
-let circle = document.querySelector('.circle');
-let slider = document.querySelector('.slider');
-let list = document.querySelector('.list');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
-let items = document.querySelectorAll('.list .item');
-let count = items.length;
+let c = document.querySelector('.c');
+let s = document.querySelector('.slider');
+let l = document.querySelector('.list');
+let p = document.getElementById('prev');
+let n = document.getElementById('next');
+let i = document.querySelectorAll('.list .item');
+let co = i.length;
 let active = 1;
 let leftTransform = 0;
-let width_item = items[active].offsetWidth;
+let width_item = i[active].offsetWidth;
 
-next.onclick = () => {
-    active = active >= count - 1 ? count - 1 : active + 1;
+n.onclick = () => {
+    active = active >= co - 1 ? co - 1 : active + 1;
     runCarousel();
 }
-prev.onclick = () => {
+p.onclick = () => {
     active = active <= 0 ? active : active - 1;
     runCarousel();
 }
 function runCarousel() {
-    prev.style.display = (active == 0) ? 'none' : 'block';
-    next.style.display = (active == count - 1) ? 'none' : 'block';
+    p.style.display = (active == 0) ? 'none' : 'block';
+    n.style.display = (active == co - 1) ? 'none' : 'block';
 
 
     let old_active = document.querySelector('.item.active');
     if(old_active) old_active.classList.remove('active');
-    items[active].classList.add('active');
+    i[active].classList.add('active');
 
      leftTransform = width_item * (active - 1) * -1;
-    list.style.transform = `translateX(${leftTransform}px)`;
+    l.style.transform = `translateX(${leftTransform}px)`;
 }
 runCarousel();
 
 
 // Set Text on a Circle
-let textCircle = circle.innerText.split('');
-circle.innerText = '';
+let textCircle = c.innerText.split('');
+c.innerText = '';
 textCircle.forEach((value, key) => {
     let newSpan =  document.createElement("span");
     newSpan.innerText = value;
     let rotateThisSpan = (360 / textCircle.length) * (key + 1);
     newSpan.style.setProperty('--rotate', rotateThisSpan + 'deg');
-    circle.appendChild(newSpan); 
+    c.appendChild(newSpan); 
 });
