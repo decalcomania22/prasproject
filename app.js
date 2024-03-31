d88d54ca357380ae9b1dc7e1146acc81b2212d39
Suhani Singh <aayushisingh7438@gmail.com>
2024-03-31 23:06:01 +0530

    near final

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express=require('express');
 const path=require('path');
 const fs=require('fs');
 const mongodb=require('mongodb');
+const bcrypt=require('bcryptjs');
+// const multer=require('multer');
 const db=require('./data/database');
+const upload=multer({dest:'presimages'});
 
 const app=express();
 
@@ -14,6 +17,41 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 
 
+app.get('/signuppage',function(req,res){
+    const pathtosignuppage=path.join(__dirname,'views','signup.html');
+
+    res.sendFile(pathtosignuppage);
+
+
+});
+
+
+
+app.post('/signup',async function(req,res){
+const givenemail=req.body.enteredemail;
+const givenpassword=req.body.enteredpassword;
+
+const hashedpassword=await bcrypt.hash(givenpassword,12);
+
+const user={
+email:givenemail,
+password:hashedpassword
+};
+
+const userarray=await db.getDb().collection('users').find().project({email:1,_id:0}).toArray();
+console.log(userarray);
+for(a of userarray){
+    if(a.email==givenemail){
+        // alert('A user with this emailId already exists.');
+       return res.redirect('/signuppage');
+    }
+}
+
+await db.getDb().collection('users').insertOne(user);
+return res.redirect('/login');
+
+});
+
 
 
 app.get('/login',function(req,res){
@@ -21,6 +59,32 @@ app.get('/login',function(req,res){
 
     res.sendFile(pathtologinpage);
 });
+
+
+
+
+
+app.post('/loginform',async function(req,res){
+
+const enteredloginemail=req.body.elogaddress;
+const enteredloginpassword=req.body.elogpassword;
+
+const boolvar=await db.getDb().collection('users').findOne({email:enteredloginemail});
+if(!boolvar){
+    console.log('Could not log in!unkonwn email');
+    return res.redirect('/login');
+}
+const passwordsareequal=await bcrypt.compare(enteredloginpassword,boolvar.password);
+
+if(!passwordsareequal){
+    console.log('Could not log in! wrong password');
+    return res.redirect('/login');
+}
+res.redirect('/');
+
+});
+
+
 
 app.get('/',function(req,res){
     const pathtoindexpage=path.join(__dirname,'views','index.html');
@@ -82,8 +146,7 @@ app.get('/labtests',function(req,res){
 app.post('/labsearch',async function(req,res){
     const testpin=req.body.testplace;
     const sym=req.body.Symtest;
-    console.log(testpin);
-    console.log(sym);
+ 
     
     const testdata=await db.getDb().collection('labtest').find({$and:[{pincode:testpin},{symptom:sym}]}).project({name:1,price:1,_id:0}).toArray();
     res.render('labtestgrid',{test:testdata}); 
@@ -98,13 +161,32 @@ app.get('/medicines',function(req,res){
 
 app.post('/searchmeds',async function(req,res){
 const prod=req.body.productname;
-console.log(prod);
+
 
 const meddata=await db.getDb().collection('meds').find({$or:[{type:prod},{brandname:prod},{name:prod}]}).project({name:1,price:1,image:1,_id:0}).toArray();
 res.render('productsgrid',{med:meddata}); 
 
 });
+// app.get('/uploadfile',function(req,res){
+//     const pathtouploadpage=path.join(__dirname,'views','uploadpres.html');
 
+//     res.sendFile(pathtouploadpage);
+
+// });
+
+app.post('/reviewsubmit',async function(req,res){
+    const newreview=req.body.review;
+    await  db.getDb().collection('reviews').insertOne({experience:newreview});
+    res.redirect('/');
+});
+
+
+// app.post('/presupload',upload.single('prescription'),function(req,res){
+//     const uploadedimg=req.file;
+    
+
+    
+// });
 
 db.connectToDatabase().then(function (){
 app.listen(3000);
